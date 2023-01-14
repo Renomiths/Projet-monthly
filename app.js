94efcecb1abfff2c93aeb6f8117b4876dd95f77b
Pascal Abouadaou <pascal.abouadaou@gmail.com>
2023-01-14 23:52:45 +0100

    test

--- app.js
+++ app.js
@@ -20,16 +20,23 @@ function addNumbers() {
 // Ajout d'un listener sur le bouton pour lancer la fonction
 document.querySelector("#button").addEventListener("click", addNumbers);
 
+let password = "mdp";
+let validPassword = "secret";
 let popup = document.querySelector("#popup");
 let popupBtn = document.querySelector("#popup-btn");
 let saveBtn = document.querySelector("#save-btn");
 let cancelBtn = document.querySelector("#cancel-btn");
 let cancelInfoBtn = document.querySelector("#cancel-info-btn");
-let password = "mdp";
-let validPassword = "secret";
 let mdpdisable = document.querySelector("#mdpdisable");
-let infoForm = document.querySelector("#TauxHorraire");
-let hidden = document.querySelector("#info-form")
+let hidden = document.querySelector("#info-form");
+let TauxH = document.querySelector("#TauxHorraire");
+
+let IndemEntr = document.querySelector("#IndemnEntretien");
+let TauxR = document.querySelector("#TauxRepas");
+let SlrBaseContr = document.querySelector("#SalaireBaseContrat");
+let NbrHMatin = document.querySelector("#NbrHeureMatin");
+let NbrHAprem = document.querySelector("#NbrHeureAprem");
+let NbrHJour = document.querySelector("#NbrHeurejournee");
 
 // Ajout d'un listener sur le bouton pour ouvrir la pop-up
 popupBtn.addEventListener("click", function () {
@@ -49,23 +56,44 @@ submitBtn.addEventListener("click", function () {
 
 // Ajout d'un listener sur le bouton pour enregistrer les informations
 
-
 saveBtn.addEventListener("click", function () {
   // Récupération de la valeur saisie dans les champs de saisie
-  let TauxHor = infoForm.value;
+
+  TauxHor = TauxH.value;
+  IndemEntr = IndemEntr.value;
+  TauxR = TauxR.value;
+  SlrBaseContr = SlrBaseContr.value;
+  NbrHMatin = NbrHMatin.value;
+  NbrHAprem = NbrHAprem.value;
+  NbrHJour = NbrHJour.value;
+
   // Fermeture de la pop-up
   popup.style.display = "none";
 
   // Mise à jour des calculs sur la page initiale en utilisant la variable storedInfo
   console.log(TauxHor);
+  console.log(IndemEntr);
+  console.log(TauxR);
+  console.log(SlrBaseContr);
+  console.log(NbrHMatin);
+  console.log(NbrHAprem);
+  console.log(NbrHJour);
 
-  let formData  = { TauxHor : TauxHor};
+  let formData = {
+    TauxHor: TauxHor,
+    IndemEntr: IndemEntr,
+    TauxR: TauxR,
+    SlrBaseContr: SlrBaseContr,
+    NbrHMatin: NbrHMatin,
+    NbrHAprem: NbrHAprem,
+    NbrHJour: NbrHJour,
+  };
+
   localStorage.setItem("formData", JSON.stringify(formData));
-
 });
 
 let storedData = JSON.parse(localStorage.getItem("formData"));
-console.log(storedData)
+console.log(storedData);
 
 // Ajout d'un listener sur le bouton pour annuler
 cancelBtn.addEventListener("click", function () {
